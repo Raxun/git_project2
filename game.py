2e0f2ba62ba2be4e9af3d228b9e752bfab0cda01
Raxun <tatarinovandry@mai.ru>
2021-12-24 18:00:47 +0300

    1

---
+++ game.py
@@ -0,0 +1,196 @@
+import os
+import sys
+import pygame
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+pygame.init()
+screen_size = (990, 600)
+screen = pygame.display.set_mode(screen_size)
+FPS = 60
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png'),
+    'spawn': load_image('spawn.png')
+}
+player_image = load_image('mario.png')
+
+
+class ScreenFrame(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.rect = (0, 0, 990, 600)
+
+
+class SpriteGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+
+    def get_event(self, event):
+        for sprite in self:
+            sprite.get_event(event)
+
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, group):
+        super().__init__(group)
+        self.rect = None
+
+    def get_event(self, event):
+        pass
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(sprite_group)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(hero_group)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 8, tile_height * pos_y + 3)
+        self.pos = (pos_x, pos_y)
+
+    def move(self, x, y):
+        self.pos = (x, y)
+        self.rect = self.image.get_rect().move(
+            tile_width * self.pos[0] + 10, tile_height * self.pos[1] + 3)
+
+
+player = None
+running = True
+clock = pygame.time.Clock()
+sprite_group = SpriteGroup()
+hero_group = SpriteGroup()
+tile_width = tile_height = 30
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), screen_size)
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: list(x.ljust(max_width, '.')), level_map))
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '*':
+                Tile('empty', x, y)
+            elif level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('spawn', x, y)
+                new_player = Player(x, y)
+                level[y][x] == '.'
+    return new_player, x, y
+
+
+def move(hero, movement):
+    x, y = hero.pos
+    symbols_true = ['.', '@', '*']
+    '''print(level_map[x][y - 1])
+    print(level_map[x][y + 1])
+    print(level_map[x - 1][y])
+    print(level_map[x][y + 1])
+    print()'''
+    if movement == 'up':
+        if y > 0 and (level_map[y - 1][x] in symbols_true):
+            hero.move(x, y - 1)
+    elif movement == 'down':
+        if y < max_y - 1 and (level_map[y + 1][x] in symbols_true):
+            hero.move(x, y + 1)
+    elif movement == 'left':
+        if x > 0 and (level_map[y][x - 1] in symbols_true):
+            hero.move(x - 1, y)
+    elif movement == 'right':
+        if x < max_x - 1 and (level_map[y][x + 1] in symbols_true):
+            hero.move(x + 1, y)
+    x, y = hero.pos
+    if level_map[y][x] == '*':
+        pass  # level completed
+
+
+start_screen()
+level_map = load_level('map.map')
+'''tile_width = tile_height = int((''.join(load_level('map.map')[0]))[0:2])
+Изменять Размеры КакУказаноВ 1 строчке файла'''
+hero, max_x, max_y = generate_level(level_map)
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                move(hero, 'up')
+            elif event.key == pygame.K_DOWN:
+                move(hero, 'down')
+            elif event.key == pygame.K_LEFT:
+                move(hero, 'left')
+            elif event.key == pygame.K_RIGHT:
+                move(hero, 'right')
+    screen.fill(pygame.Color(37, 9, 54))
+    sprite_group.draw(screen)
+    hero_group.draw(screen)
+    clock.tick(FPS)
+    pygame.display.flip()
+pygame.quit()
